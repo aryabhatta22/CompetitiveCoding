61b12da5d3a611f5420ce33ff662d3aa6b5fbb59
aryabhatta <tarun.hive@hotmail.com>
2020-04-05 23:45:33 +0530

    Solved in O(n)

---
+++ LeetCode/LeetCode-30days-challenge/Best time to but and sell stock II.cpp
@@ -0,0 +1,61 @@
+/***************************************
+
+Description: Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Example 2:
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+
+***************************************/
+
+int maxProfit(vector<int>& prices) {
+        if(prices.size() == 1)
+            return 0;
+        else {
+            int buy=0;
+            int sell = 1;
+            int profit =0;
+             while(sell < prices.size()) {
+                 
+                 if(sell >= prices.size() -1){
+                     if(prices[sell] > prices[buy])
+                         return profit+(prices[sell] - prices[buy]);
+                    else return profit;
+                 } else {
+                     if(prices[sell] <= prices[buy]) {
+                         sell ++;
+                         buy ++;
+                     } else if(prices[sell] >= prices[sell +1]) {
+                         profit = profit + (prices[sell] - prices[buy]);
+                         buy = sell +1;
+                         sell = buy +1;
+                     } else
+                         sell ++ ;
+                 }
+                 
+                 
+             }
+               return profit;     
+    }
+
+    }
