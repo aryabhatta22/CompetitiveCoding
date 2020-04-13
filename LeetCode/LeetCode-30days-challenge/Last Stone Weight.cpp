2ea802fcd51d230c5c66f6f7d5adff6f6dc4fb69
aryabhatta <tarun.hive@hotmail.com>
2020-04-13 10:27:25 +0530

    solved in O(n2): O(n) required

---
+++ LeetCode/LeetCode-30days-challenge/Last Stone Weight.cpp
@@ -0,0 +1,44 @@
+/***************************************
+Description: We have a collection of stones, each stone has a positive integer weight.
+
+Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+If x == y, both stones are totally destroyed;
+If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+Example 1:
+
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+
+***************************************/
+
+
+int lastStoneWeight(vector<int>& stones) {
+        if(stones.size() == 1)
+            return stones[0];
+        while(stones.size() >= 1) {
+            sort(stones.begin(), stones.end());
+            int last = stones.size() -1;
+            int secondLast = stones.size() -2;
+            
+            if(stones[last] == stones[secondLast])
+                stones.erase(stones.begin()+ secondLast, stones.begin()+ last+1);
+            else {
+                int newWeight = stones[last] - stones[secondLast];
+                stones.erase(stones.begin()+ secondLast, stones.begin()+ last+1);
+                stones.push_back(newWeight);
+            }
+            
+            if(stones.size() == 1)
+                return stones[0];
+        }
+        
+        return 0;
+    }
