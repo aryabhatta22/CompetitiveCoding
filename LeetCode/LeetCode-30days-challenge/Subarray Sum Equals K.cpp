e90d936641f1e496409b621a4681508103cb3134
aryabhatta <tarun.hive@hotmail.com>
2020-04-22 19:30:17 +0530

    Array: Medium: O(n)

--- LeetCode/LeetCode-30days-challenge/Subarray Sum Equals K.cpp
+++ LeetCode/LeetCode-30days-challenge/Subarray Sum Equals K.cpp
@@ -38,3 +38,23 @@ public:
         return count;
     }
 };
+
+
+// O(n) ... Optimal solution
+
+class Solution {
+    
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0;
+		int sum = 0;
+		unordered_map<int, int> map;
+		map[0] = 1;
+		for (int num : nums) {
+			sum += num;
+			count += map[sum - k];
+			map[sum]++;
+		}
+		return count;
+    }
+};
