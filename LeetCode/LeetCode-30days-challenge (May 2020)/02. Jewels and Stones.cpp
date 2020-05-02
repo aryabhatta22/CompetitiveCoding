47408c076a29467c1b6aeeaa96f3d280f1903073
aryabhatta <tarun.hive@hotmail.com>
2020-05-02 23:17:02 +0530

    Easy: array, hashset: solved

---
+++ LeetCode/LeetCode-30days-challenge (May 2020)/02. Jewels and Stones.cpp
@@ -0,0 +1,32 @@
+/***************************************
+Description: You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+***************************************/
+
+int numJewelsInStones(string J, string S) {
+        
+        if(J.length() == 0 || S.length() == 0)
+            return 0;
+        
+        unordered_set<char> jewel;
+        int result = 0;
+        for(char el: J)
+            jewel.insert(el);
+        
+        for(char el: S)
+            if(jewel.find(el) != jewel.end())
+                result++;
+        
+        return result;
+            
+    }
