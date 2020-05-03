cffa08356e3d1bcf7c6aeb5bbf1a686c1cc37a60
aryabhatta <tarun.hive@hotmail.com>
2020-05-04 03:20:39 +0530

    Easy: array, hash set: solved

--- LeetCode/Solutions/003. Contains Duplicate.cpp
+++ LeetCode/Solutions/003. Contains Duplicate.cpp
@@ -17,3 +17,19 @@ Input: [1,1,1,3,3,4,3,2,4,2]
 Output: true
  
 ***************************************/
+
+bool containsDuplicate(vector<int>& nums) {
+        
+        if(nums.size() == 0 ||  nums.size() == 1)
+            return false;
+        
+        unordered_set<int> _set;
+        for(int el: nums) {
+            if(_set.find(el) != _set.end())
+                return true;
+            else
+                _set.insert(el);
+        }
+        
+        return false;
+    }
