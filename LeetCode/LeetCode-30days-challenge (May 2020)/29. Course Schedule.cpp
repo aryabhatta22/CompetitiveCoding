a68f3607462646144840beaf8e8e49d2790a1ef5
aryabhatta <tarun.hive@hotmail.com>
2020-05-29 21:36:14 +0530

    Hard: graph, sort, dfs, bfs : solved

---
+++ LeetCode/LeetCode-30days-challenge (May 2020)/29. Course Schedule.cpp
@@ -0,0 +1,67 @@
+/***************************************
+Description: There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+ 
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+
+***************************************/
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+         graph g = buildGraph(numCourses, prerequisites);
+        vector<bool> todo(numCourses, false), done(numCourses, false);
+        for (int i = 0; i < numCourses; i++) {
+            if (!done[i] && !acyclic(g, todo, done, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+private:
+    
+    typedef vector<vector<int>> graph;
+    
+    static graph buildGraph(int numCourses, vector<vector<int>>& prerequisites) {
+        graph g(numCourses);
+        for (auto p : prerequisites) {
+            g[p[1]].push_back(p[0]);
+        }
+        return g;
+    }
+    
+    static bool acyclic(graph& g, vector<bool>& todo, vector<bool>& done, int node) {
+        if (todo[node]) {
+            return false;
+        }
+        if (done[node]) {
+            return true;
+        }
+        todo[node] = done[node] = true;
+        for (int v : g[node]) {
+            if (!acyclic(g, todo, done, v)) {
+                return false;
+            }
+        }
+        todo[node] = false;
+        return true;
+    }
+};
