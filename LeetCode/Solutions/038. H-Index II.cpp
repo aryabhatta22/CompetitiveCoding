3d6dad2996125a9796ec903ccf0355dfdc685a78
aryabhatta <tarun.hive@hotmail.com>
2020-06-19 11:19:51 +0530

    Easy: Binary Search: solved

---
+++ LeetCode/Solutions/038. H-Index II.cpp
@@ -0,0 +1,18 @@
+int hIndex(vector<int>& citations) {
+        if (citations.empty()) return 0;
+		int n = citations.size();
+		int left = 0, right = n - 1;
+		while (left <= right) {
+			int mid = (left + right) >> 1;
+			if (n - mid == citations[mid]) {
+				return citations[mid];
+			}
+			else if (n - mid > citations[mid]) {
+				left = mid + 1;
+			}
+			else {
+				right = mid - 1;
+			}
+		}
+		return n - left;
+    }
